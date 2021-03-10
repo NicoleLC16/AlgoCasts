c899fa95d024581169aef32d7a45c74c29d7a4eb
NicoleLC16 <asalwaysnicole@outlook.com>
2021-03-09 19:12:48 -0800

    width tree practice

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let currentTier = [root];
+  let counters = [];
+
+  while (currentTier.length) {
+    counters.push(currentTier.length); //push length of tier into counters
+    currentTier = currentTier.reduce(
+      (tempArr, node) => tempArr.concat(node.children),
+      //concat instead of push because push will add children as array instead of single elements
+      //ex: push [1,2,3,[4,5,6]]
+      //concat merges arrays
+      []
+    );
+    //unpacking children of each node into a temporary array.
+    //then while loop will push length of tier into counters
+  }
+
+  return counters;
+}
 
 module.exports = levelWidth;
